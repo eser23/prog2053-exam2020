63bd1e26b024e8dc2a750b211bb59066ac6e8d2b
eser23 <haaftii@gmail.com>
2020-12-17 11:21:21 +0100

    Update edit-user.js

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -6,6 +6,34 @@ class EditUser extends LitElement {
       user: { type: Object }
     };
   }
+  
+  render() {
+    return html`
+    <head>
+    </head>
+    <form onsubmit="javascript: return false;" id=user" method="POST"
+    <div>
+      <label for="firstName">First name</label>
+      <input id=firstName" name="firstName" type="text" value={this.user.firstName}">
+      </div>
+    <div>
+    <label for="lastName"<Last name</label>
+    <input id="lastName name="lastname" type="text" value="${this.user.lastName"}>
+    </div>
+    <div>
+    <label for ="email"E-mail address</label>
+    <input id ="uname" name="uname" type="text" value="${this.user.uname}>
+    </div>
+    <div>
+    <label for ="password"<Password"</label>
+    <input id="password" name="password" type="password" value="">
+    <div>
+    <input type="submit" @click=${this.updateUser} id="submit" name ="edituser" value="Submit"<>/input>
+</form>
+;
+} 
+}
+    
 
   // din kode her
     updateUser(e) {
